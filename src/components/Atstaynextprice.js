d4d3115048d8fb83a40952a1faece20eed5c8248
Atraski <atraskiwebsite@gmail.com>
2023-10-23 15:34:45 +0530

    first commit

---
+++ src/components/Atstaynextprice.js
@@ -0,0 +1,178 @@
+export const productData6=[
+    {
+        id:1,
+        price:8000
+    },
+
+    {
+        id:2,
+        price:8000
+    },
+
+    {
+        id:3,
+        price:13000
+    },
+
+    {
+        id:4,
+        price:13000
+    },
+
+    {
+        id:5,
+        price:13000
+    },
+
+    {
+        id:6,
+        price:13000
+    },
+
+    {
+        id:7,
+        price:13000
+    },
+
+    {
+        id:8,
+        price:8000
+    },
+
+    {
+        id:9,
+        price:8000
+    },
+
+    {
+        id:10,
+        price:8000
+    },
+
+    {
+        id:11,
+        price:8000
+    },
+
+    {
+        id:12,
+        price:8000
+    },
+
+    {
+        id:13,
+        price:8000
+    },
+
+    {
+        id:14,
+        price:5500
+    },
+
+    {
+        id:15,
+        price:12665
+    },
+
+    {
+        id:16,
+        price:12665
+    },
+
+    {
+        id:17,
+        price:12665
+    },
+
+    {
+        id:18,
+        price:12665
+    },
+
+    {
+        id:19,
+        price:12665
+    },
+
+    {
+        id:20,
+        price:8000
+    },
+
+    {
+        id:21,
+        price:8000
+    },
+
+    {
+        id:22,
+        price:8000
+    },
+
+    {
+        id:23,
+        price:17400
+    },
+
+    {
+        id:24,
+        price:17400
+    },
+
+    {
+        id:25,
+        price:7000
+    },
+
+    {
+        id:26,
+        price:7000
+    },
+
+    {
+        id:27,
+        price:7000
+    },
+
+    {
+        id:28,
+        price:7000
+    },
+
+    {
+        id:29,
+        price:2900
+    },
+
+    {
+        id:30,
+        price:2900
+    },
+
+    {
+        id:31,
+        price:2900
+    },
+
+    {
+        id:32,
+        price:2900
+    },
+
+    {
+        id:33,
+        price:2900
+    },
+
+    {
+        id:34,
+        price:2900
+    },
+
+    {
+        id:35,
+        price:4200
+    },
+
+    
+]
